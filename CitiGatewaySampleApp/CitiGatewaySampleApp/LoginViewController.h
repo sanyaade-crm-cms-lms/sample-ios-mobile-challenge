66107bf65548503c12c9ba1888f2dbf5a9d17a6b
MaxiSanchezAP <maximiliano@soportes-Mac-mini.local>
2015-03-06 15:09:44 -0300

    Added logo and made navbar blue.

--- CitiGatewaySampleApp/CitiGatewaySampleApp/LoginViewController.h
+++ CitiGatewaySampleApp/CitiGatewaySampleApp/LoginViewController.h
@@ -8,12 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
-@interface LoginViewController : UIViewController
+@interface LoginViewController : UIViewController <UITextFieldDelegate>
 
+@property (weak, nonatomic) IBOutlet UIImageView *logoImageView;
 @property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
 @property (weak, nonatomic) IBOutlet UIButton *loginButton;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicatorView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *keyboardConstraint;
 
 - (IBAction)loginButtonTouched;
 
--- CitiGatewaySampleApp/CitiGatewaySampleApp/LoginViewController.m
+++ CitiGatewaySampleApp/CitiGatewaySampleApp/LoginViewController.m
@@ -13,7 +13,11 @@
 #import <APSDK/LoginInfo+Remote.h>
 #import "ContextManager.h"
 
+#define kLoginButtonToKeyboardOffset 25.0
+
 @interface LoginViewController ()
+
+@property (assign, nonatomic) CGFloat defaultKeyboardConstraintConstant;
 
 @end
 
@@ -21,7 +25,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.defaultKeyboardConstraintConstant = self.keyboardConstraint.constant;
+    
+    UITapGestureRecognizer *singleTapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(singleTappedView)];
+    [self.view addGestureRecognizer:singleTapGestureRecognizer];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillBeShown:)
+                                                 name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillBeHidden:)
+                                                 name:UIKeyboardWillHideNotification object:nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -29,15 +44,39 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)keyboardWillBeShown:(NSNotification*)notification {
+    NSDictionary* info = [notification userInfo];
+    CGSize endSize = [[info objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue].size;
+    UIViewAnimationOptions animationCurve = [[info objectForKey:UIKeyboardAnimationCurveUserInfoKey] intValue];
+    NSTimeInterval animationDuration = [[info objectForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    self.keyboardConstraint.constant = fmaxf(self.defaultKeyboardConstraintConstant, -(self.view.bounds.size.height/2 - endSize.height - kLoginButtonToKeyboardOffset));
+    [UIView animateWithDuration:animationDuration delay:0 options:animationCurve animations:^{
+        [self.view layoutIfNeeded];
+        if (self.logoImageView.frame.origin.y < 0) {
+            self.logoImageView.alpha = 0;
+        }
+    } completion:^(BOOL finished) {}];
 }
-*/
+
+- (void)keyboardWillBeHidden:(NSNotification*)notification {
+    NSDictionary* info = [notification userInfo];
+    UIViewAnimationOptions animationCurve = [[info objectForKey:UIKeyboardAnimationCurveUserInfoKey] intValue];
+    NSTimeInterval animationDuration = [[info objectForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    self.keyboardConstraint.constant = self.defaultKeyboardConstraintConstant;
+    [UIView animateWithDuration:animationDuration delay:0 options:animationCurve animations:^{
+        [self.view layoutIfNeeded];
+        self.logoImageView.alpha = 1.0;
+    } completion:^(BOOL finished) {}];
+}
+
+- (void)singleTappedView {
+    [self.view endEditing:YES];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self.view endEditing:YES];
+    return YES;
+}
 
 - (IBAction)loginButtonTouched {
     self.loginButton.hidden = YES;
@@ -58,5 +97,15 @@
         }
     }];
 }
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
